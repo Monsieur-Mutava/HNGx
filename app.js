95b93727f0774607075036d7349fb560fd71d8c3
Mutava <125569431+Monsieur-Mutava@users.noreply.github.com>
2023-09-08 23:01:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,36 +2,38 @@ const express = require('express');
 const app = express();
 
 app.get('/api', (req, res) => {
-    const slackName = req.query.slack_name;
+    // Get query parameters from the request
+    const slack_name = req.query.slack_name;
     const track = req.query.track;
 
-    if (!slackName || !track) {
+    // Check if required parameters are present
+    if (!slack_name || !track) {
         return res.status(400).json({ error: 'slack_name and track are required parameters.' });
     }
 
     // Get the current day of the week
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const currentDay = daysOfWeek[new Date().getDay()];
+    const current_day = daysOfWeek[new Date().getDay()];
 
     // Get the current UTC time within +/-2 minutes
     const now = new Date();
-    const utcTime = new Date(now.getTime() + (now.getTimezoneOffset() * 60000)).toISOString();
+    const utc_time = new Date(now.getTime() + (now.getTimezoneOffset() * 60000)).toISOString();
 
     // Construct the response object
     const response = {
-        slackName,
-        currentDay,
-        utcTime,
+        slack_name,
+        current_day,
+        utc_time,
         track,
-        githubFileUrl: 'https://github.com/Monsieur-Mutava/HNGx/blob/main/app.js',
-        githubRepoUrl: 'https://github.com/Monsieur-Mutava/HNGx.git',
-        statusCode: 200,
+        github_file_url: 'https://github.com/Monsieur-Mutava/HNGx/blob/main/app.js',
+        github_repo_url: 'https://github.com/Monsieur-Mutava/HNGx.git',
+        status_code: 200,
     };
 
     res.json(response);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
